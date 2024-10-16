d074e0fde426723839e6fe4963c8b1441ea1d524
sairakausar <sairakausar3@gmail.com>
2024-10-16 17:03:33 +0000

    make agent full agent with general Intellence which acccording to user QUestion give Answer from website , 1 Tool from website abd . 2nd tool from real time data

--- langchain_Tools.py
+++ langchain_Tools.py
@@ -257,64 +257,135 @@
 # output = chain.invoke("vmy first number is 10 second number should be minust of first number by 2")
 # print("output", output)
 
-#   ,,,,,,,,,,,,,,,,,,,,,,,,,,,,........................
+#   ,,,,,,,,,,,,,,,,,,,,,,,6,,,,,........................
 
-from langchain.agents import initialize_agent, AgentType
+# from langchain.agents import initialize_agent, AgentType
 
-from langchain_google_genai import GoogleGenerativeAI
+# from langchain_google_genai import GoogleGenerativeAI
 
-from langchain_core.tools import tool
-
-
-llm = GoogleGenerativeAI(model="gemini-1.5-flash", api_key="AIzaSyAidgZgpXu9mvAPEMSGGeiRaAdWF05rd5s")
+# from langchain_core.tools import tool
 
 
-@tool
-def add_numbers_tools(input_data: str) -> str:
-    """addition of two numbers"""
-    print("add number Tool ", input_data)
+# llm = GoogleGenerativeAI(model="gemini-1.5-flash", api_key="AIzaSyAidgZgpXu9mvAPEMSGGeiRaAdWF05rd5s")
+
+
+# @tool
+# def add_numbers_tools(input_data: str) -> str:
+#     """addition of two numbers"""
+#     print("add number Tool ", input_data)
     
-    try: 
-        print("function parameter",input_data)
+#     try: 
+#         print("function parameter",input_data)
 
-        numbers =input_data.split(",")
+#         numbers =input_data.split(",")
 
-        print("after split",numbers)
+#         print("after split",numbers)
 
         
 
-    except Exception as e:
-        return "No number found"
+#     except Exception as e:
+#         return "No number found"
 
-    num1 = int(numbers[0])
+#     num1 = int(numbers[0])
 
-    print("first number",num1)
+#     print("first number",num1)
     
-    num2 = int(numbers[1])
+#     num2 = int(numbers[1])
         
-    print("second number",num2)
+#     print("second number",num2)
 
-    result= num1+num2
+#     result= num1+num2
 
-    print("result",result)
+#     print("result",result)
 
-    return f"The sum of {num1} and  {num2} is {result}"
+#     return f"The sum of {num1} and  {num2} is {result}"
 
-    #   #    or
-    #  #  result "The sum of", {num1}, "and", {num2}, "is", {result}
+#     #   #    or
+#     #  #  result "The sum of", {num1}, "and", {num2}, "is", {result}
 
-@tool
-def get_weather_tool(city: str) -> str:
-    """ check weather info from llm """
-    return f"the waether of {city} in 25C"
+# @tool
+# def get_weather_tool(city: str) -> str:
+#     """ check weather info from llm """
+#     return f"the waether of {city} in 25C"
 
 
-agent = initialize_agent(
-    tools = [add_numbers_tools ,  get_weather_tool],
-    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-    llm=llm,
-    verbose=True,
-    max_iterations=2,
+# agent = initialize_agent(
+#     tools = [add_numbers_tools ,  get_weather_tool],
+#     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+#     llm=llm,
+#     verbose=True,
+#     max_iterations=2,
+# )
+
+# agent.run("what is the weather of faisalabad")
+# # # ...................7.....................
+
+from langchain.agents import AgentExecutor
+from langchain.agents import create_tool_calling_agent
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import FAISS   # FAISS = DATA base
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.tools.retriever import create_retriever_tool
+from langchain_community.chat_message_histories import ChatMessageHistory #manage Chat Memory we make list and it few message save
+from langchain_core.runnables.history import RunnableWithMessageHistory  
+from langchain import hub
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#Intialize the LLM with GoogleGenerativeAI
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=os.getenv("GOOGLE_API_KEY"))
+
+search = TavilySearchResults(tavily_api_key=os.getenv("TAVILY_API_KEY"))
+
+
+ # this also a tool
+
+loader =WebBaseLoader("https://www.techloset.com/")
+docs = loader.load()
+documents = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=200
+).split_documents(docs)
+
+vector = FAISS.from_documents(documents, GoogleGenerativeAIEmbeddings(model="models/embedding-001"))
+
+content = vector.as_retriever()
+
+retriever_tool = create_retriever_tool(      # three things
+    content, # retriver .....data collect
+    "techloset_search",  #  name 
+    "Search for any questions about Techloset Solutiojs, you must use this tool" #prompt  kab ya tool call ho sakta ha
 )
 
-agent.run("what is the weather of faisalabad")
+tools = [search, retriever_tool]     # ya 2 tool hain is main
+
+# we make agent crator and execut agant
+
+prompt = hub.pull("hwchase17/openai-functions-agent")
+
+agent = create_tool_calling_agent(
+    llm,
+    tools,
+    prompt
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+
+message_history = ChatMessageHistory()
+
+agent_with_chat_history = RunnableWithMessageHistory(
+    agent_executor,
+    lambda session_id: message_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+)
+
+while True:
+    agent_with_chat_history.invoke(
+        {"input": input("How can I help you today? : ")},
+        config={"configurable": {"session_id":"test123"}},
+    )
